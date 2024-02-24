6a184a45226e2b81c39440545c831641778b7cb4
sikemausa <mikejsausa@gmail.com>
2024-02-23 21:32:09 -0700

    Fix build error

--- app/page.js
+++ app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Nah we're not doing that
+            Nah were not doing that
           </p>
         </a>
 
